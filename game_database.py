406af1da70e4d6ea140b6fcf74cf57806e14639d
codewithdhruval <110345599+codewithdhruval@users.noreply.github.com>
2023-07-21 16:40:05 +0530

    game_database.py

---
+++ game_database.py
@@ -0,0 +1,56 @@
+data = [
+    {
+        'name': "Jenny's Lectures",
+        'follower_count':1,
+        'description':'YouTuber',
+        'country':'India'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count':600,
+        'description':'Footballer',
+        'country':'Portugal'
+    },
+    {
+        'name': 'Narendra Modi',
+        'follower_count':90,
+        'description':'Prime Minister',
+        'country':'India'
+    },
+    {
+        'name': 'Fit Tuber',
+        'follower_count':7,
+        'description':'YouTuber',
+        'country':'India'
+    },
+    {
+        'name': 'Selena Gomez',
+        'follower_count':420,
+        'description':'Musician and actress',
+        'country':'United States'
+    },
+    {
+        'name': 'Pankaj Tripathi',
+        'follower_count':5,
+        'description':'Actor',
+        'country':'India'
+    },
+    {
+        'name': 'Lionel Messi',
+        'follower_count':480,
+        'description':'Footballer',
+        'country':'Argentina'
+    },
+    {
+        'name': 'Beyonce',
+        'follower_count':315,
+        'description':'Musician',
+        'country':'United States'
+    },
+     {
+        'name': 'T-Series',
+        'follower_count':245,
+        'description':'Music Label & Movie Studio',
+        'country':'India'
+    }
+]
